f58e1c89b914e837e11c368fff9ac6f68fe736e8
최유진 <yjyj0774@gmail.com>
2025-03-02 16:31:30 +0900

    css 파일 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from "./app"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <App />
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
 );
+
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.js|ts|tsx|jsx$/,
