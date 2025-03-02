cbd9842b10bc8cfa217b3ab12d8417eaa2c671f6
최유진 <yjyj0774@gmail.com>
2025-03-02 16:30:16 +0900

    ProjectIcon 컴포넌트 생성

---
+++ src/components/ProjectIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectIcon: React.FC = () => {
+  return (
+    <div>
+      <h1>프로젝트 이름</h1>
+      <p>자세히 보기</p>
+    </div>
+  );
+};
+
+export default ProjectIcon;
--- src/pages/ProjectListPage.tsx
+++ src/pages/ProjectListPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProjectIcon from "../components/ProjectIcon";
 
 const ProjectListPage: React.FC = () => {
   return (
     <div>
       <h1>Project</h1>
+      <ProjectIcon />
     </div>
   );
 };
