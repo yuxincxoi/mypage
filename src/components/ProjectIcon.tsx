896a38a06c63305dddc1176f042ffb1404b752cf
최유진 <yjyj0774@gmail.com>
2025-03-04 18:20:24 +0900

    애니메이션 추가

--- src/components/ProjectIcon.tsx
+++ src/components/ProjectIcon.tsx
@@ -12,7 +12,7 @@ const ProjectIcon: React.FC<ProjectIconProps> = ({
     <div
       className={`w-52 h-52 bg-slate-600 rounded-full flex items-center justify-center flex-col group absolute transition-transform ${
         isBlurred ? "opacity-90 blur-sm" : "opacity-100 blur-none"
-      } transition-all duration-500 ease-in-out`}
+      } transition-all duration-500 ease-in-out animate-float`}
       style={style}
       onClick={isBlurred ? undefined : onClick}
       data-type={projectType}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-5px)" },
+        },
+      },
+      animation: {
+        float: "float 3s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
