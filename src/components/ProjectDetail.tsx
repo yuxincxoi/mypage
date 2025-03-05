33e3b1048177e6c5134015c23cb4904a4e55b231
최유진 <yjyj0774@gmail.com>
2025-03-05 16:23:23 +0900

    임시 데이터 추가

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -11,6 +11,8 @@ interface ProjectDetailProps {
 }
 
 const ProjectDetail: React.FC<ProjectDetailProps> = ({ projects }) => {
+  const projectImages = ["사진1", "사진2", "사진3", "사진4", "사진5"];
+
   return (
     <div className="w-full bg-black text-white py-20">
       <div className="w-5/6 mx-auto text-center">
