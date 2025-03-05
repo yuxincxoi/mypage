402095b8813140bbda18106f81c993bf4c654a51
최유진 <yjyj0774@gmail.com>
2025-03-05 18:56:05 +0900

    컴포넌트 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import IntroPage from "./pages/IntroPage";
 import ProjectListPage from "./pages/ProjectListPage";
+import ContactPage from "./pages/ContactPage";
+import Footer from "../src/components/Footer";
+import Nav from "./components/Nav";
 
 const App: React.FC = () => {
   return (
     <div>
+      <Nav />
       <IntroPage />
       <ProjectListPage />
+      <ContactPage />
+      <Footer />
     </div>
   );
 };
