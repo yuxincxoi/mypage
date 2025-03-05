d33a8104c596b3b97c31be939177dafdc704f872
최유진 <yjyj0774@gmail.com>
2025-03-05 18:52:34 +0900

    contact page 생성

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ContactPage: React.FC = () => {
+  return (
+    <div className="w-full h-[65vh] bg-black text-white">
+      <div className="text-7xl font-extrabold">Contact</div>
+      <div>
+        <div>github</div>
+        <div>email</div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
