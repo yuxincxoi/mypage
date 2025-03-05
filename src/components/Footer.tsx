3dd159bc8af4e6f47e6c980f52a136fc0adf56cd
최유진 <yjyj0774@gmail.com>
2025-03-05 17:44:54 +0900

    footer 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="bg-black text-gray-300 font-thin text-sm text-center pt-40 pb-12">
+      Copyright ©2025 ChoiYujin. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/ProjectListPage.tsx
+++ src/pages/ProjectListPage.tsx
@@ -3,6 +3,7 @@ import ProjectIcon from "../components/ProjectIcon";
 import TypeChechBox from "../components/TypeCheckBox";
 import ProjectDetail from "../components/ProjectDetail";
 import ScrollToTopButton from "../components/ScrollToTopBtn";
+import Footer from "../components/Footer";
 
 const ProjectListPage: React.FC = () => {
   const [selectedType, setSelectedType] = useState<string>("all");
@@ -95,6 +96,7 @@ const ProjectListPage: React.FC = () => {
       </div>
       <ProjectDetail projects={projects} />
       <ScrollToTopButton />
+      <Footer />
     </div>
   );
 };
