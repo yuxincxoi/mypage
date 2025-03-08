d7dcded4da6fbd9126cbb6cf651d06fa22365f01
최유진 <yjyj0774@gmail.com>
2025-03-08 13:59:29 +0900

    디렉토리 구조 수정

--- src/components/project/ProjectDetail.tsx
+++ src/components/project/ProjectDetail.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectImg from "./ProjectImg";
 import Stacks from "./Stacks";
 import FadeInSection from "../../FadeInSection";
-import { ProjectDetailProps } from "../../interfaces/components/ProjectDetail.interface";
+import { ProjectDetailProps } from "../../interfaces/components/project/ProjectDetail.interface";
 
 const ProjectDetail: React.FC<ProjectDetailProps> = ({ projects }) => {
   const projectImages = ["사진1", "사진2", "사진3", "사진4", "사진5"];
--- src/interfaces/components/project/ProjectDetail.interface.ts
+++ src/interfaces/components/project/ProjectDetail.interface.ts
@@ -1,4 +1,4 @@
-import { Project } from "../Project.interface";
+import { Project } from "./Project.interface";
 
 export interface ProjectDetailProps {
   projects: Project[];
