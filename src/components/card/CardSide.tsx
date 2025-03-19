6175745ff0167e4ce95d6c66435bea9681ca3fb4
Choi YuJin <yjyj0774@gmail.com>
2025-03-19 16:20:12 +0900

    그림자 수정

--- src/components/card/CardSide.tsx
+++ src/components/card/CardSide.tsx
@@ -10,13 +10,10 @@ const CardSide: React.FC<CardSideProps> = ({
 }) => {
   return (
     <div
-      className={`absolute w-full h-full rounded-lg shadow-lg p-6 flex flex-col justify-between bg-gradient-to-r text-white`}
+      className={`absolute w-full h-full rounded-lg shadow-2xl shadow-neutral-500 p-6 flex flex-col justify-between bg-gradient-to-r text-white`}
       style={{
         backfaceVisibility: "hidden",
         transform: isBack ? "rotateY(180deg)" : "rotateY(0deg)",
-        boxShadow: isHovered
-          ? "0 15px 30px rgba(0, 0, 0, 0.4)"
-          : "0 4px 6px rgba(0, 0, 0, 0.1)",
         backgroundImage: `linear-gradient(to right, ${gradientFrom}, ${gradientTo})`,
       }}
     >
