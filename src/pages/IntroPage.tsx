30fc1507a075281a015bb9dbfdaf5eff27a468e7
Choi YuJin <yjyj0774@gmail.com>
2025-03-19 17:04:36 +0900

    intro 메세지 효과 설정

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -4,6 +4,10 @@ import { introMessage } from "../../statics/intro.static";
 
 const IntroPage: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
+  const [messageState, setMessageState] = useState({
+    first: false,
+    second: false,
+  });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,16 +20,40 @@ const IntroPage: React.FC = () => {
 
     window.addEventListener("scroll", handleScroll);
 
+    const firstTimeout = setTimeout(() => {
+      setMessageState((prev) => ({ ...prev, first: true }));
+    }, 500);
+
+    const secondTimeout = setTimeout(() => {
+      setMessageState((prev) => ({ ...prev, second: true }));
+    }, 1200);
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      clearTimeout(firstTimeout);
+      clearTimeout(secondTimeout);
     };
   }, []);
 
   return (
     <div className="w-full h-screen mb-40 flex flex-col justify-center text-9xl font-extrabold">
-      <div>
-        <div>{introMessage.first}</div>
-        <div>{introMessage.second}</div>
+      <div className="overflow-hidden">
+        <div
+          className={`transform transition-transform duration-1000 ease-out overflow-hidden ${
+            messageState.first
+              ? "translate-y-0 opacity-100"
+              : "translate-y-full opacity-0"
+          }`}
+        >
+          {introMessage.first}
+        </div>
+        <div
+          className={`transform transition-transform duration-1000 ease-out ${
+            messageState.second ? "translate-y-0" : "translate-y-full"
+          }`}
+        >
+          {introMessage.second}
+        </div>
       </div>
       <div
         className={`transition-opacity duration-300 ${
