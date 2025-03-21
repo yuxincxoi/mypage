30c4d246eedf1b1f15883a9f46e699e5408a7acb
Choi YuJin <yjyj0774@gmail.com>
2025-03-21 18:09:04 +0900

    contact page 디자인 수정

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -5,13 +5,13 @@ import { ContactMessage } from "../../statics/contact.static";
 
 const ContactPage: React.FC = () => {
   return (
-    <div className="w-full h-[65vh] text-black mt-40">
+    <div className="w-full h-[40vh] text-black mt-64">
       <div className="flex flex-col items-center justify-center">
-        <FadeInSection>
+        {/* <FadeInSection>
           <div className="text-6xl font-extrabold my-20">
             {ContactMessage.title}
           </div>
-        </FadeInSection>
+        </FadeInSection> */}
         <FadeInSection>
           <Card />
         </FadeInSection>
