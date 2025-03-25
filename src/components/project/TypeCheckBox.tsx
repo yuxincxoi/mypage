2d22e464603600070e6402259aebd01c865deb50
Choi YuJin <yjyj0774@gmail.com>
2025-03-25 18:48:31 +0900

    디자인 수정

--- src/components/project/TypeCheckBox.tsx
+++ src/components/project/TypeCheckBox.tsx
@@ -7,13 +7,11 @@ const TypeChechBox: React.FC<TypeCheckBoxProps> = ({
   onTypeChange,
 }) => {
   return (
-    <div className="flex w-72 mx-auto justify-between text-md bg-neutral-100 px-2 py-2 rounded-full opacity-95">
+    <div className="flex w-72 mx-auto text-black justify-between text-md bg-neutral-100 px-2 py-2 rounded-full opacity-95">
       <div>
         <div
-          className={`cursor-pointer text-black font-pretendard transition-all duration-500 ease-in-out w-16 h-10 text-center ${
-            selectedType === "all"
-              ? "rounded-full py-2 bg-white"
-              : "text-zinc-500 leading-10"
+          className={`cursor-pointer font-pretendard transition-all duration-500 ease-in-out w-16 h-10 text-center flex items-center justify-center ${
+            selectedType === "all" ? "rounded-full bg-white" : "text-zinc-500"
           }`}
           onClick={() => onTypeChange("all")}
         >
@@ -22,10 +20,10 @@ const TypeChechBox: React.FC<TypeCheckBoxProps> = ({
       </div>
       <div>
         <div
-          className={`cursor-pointer text-black font-pretendard transition-all duration-500 ease-in-out w-24 h-10 pr-1 text-center ${
+          className={`cursor-pointer font-pretendard transition-all duration-500 ease-in-out w-24 h-10 text-center flex items-center justify-center ${
             selectedType === "personal"
-              ? "rounded-full py-2 bg-white"
-              : "text-zinc-500 leading-10"
+              ? "rounded-full bg-white"
+              : "text-zinc-500"
           }`}
           onClick={() => onTypeChange("personal")}
         >
@@ -34,10 +32,8 @@ const TypeChechBox: React.FC<TypeCheckBoxProps> = ({
       </div>
       <div>
         <div
-          className={`cursor-pointer text-black font-pretendard transition-all duration-500 ease-in-out w-20 h-10 text-center ${
-            selectedType === "team"
-              ? "rounded-full py-2 bg-white"
-              : "text-zinc-500 leading-10"
+          className={`cursor-pointer font-pretendard transition-all duration-500 ease-in-out w-20 h-10 text-center flex items-center justify-center ${
+            selectedType === "team" ? "rounded-full bg-white" : "text-zinc-500"
           }`}
           onClick={() => onTypeChange("team")}
         >
