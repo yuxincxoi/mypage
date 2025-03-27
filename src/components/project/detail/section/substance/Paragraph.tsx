72f7fd3a07103b62fac15cbca52ba46c19b3c7dc
Choi YuJin <yjyj0774@gmail.com>
2025-03-27 17:35:57 +0900

    paragraph 컴포넌트 생성

---
+++ src/components/project/detail/section/substance/Paragraph.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ParagraphProps } from "../../../../../interfaces/components/project/detail/section/substance/Paragraph.interface";
+
+const Paragraph: React.FC<ParagraphProps> = ({ para, className }) => {
+  return <p className={` ${className}`}>{para}</p>;
+};
+
+export default Paragraph;
---
+++ src/interfaces/components/project/detail/section/substance/Paragraph.interface.ts
@@ -0,0 +1,4 @@
+export interface ParagraphProps {
+  para: string;
+  className: string;
+}
