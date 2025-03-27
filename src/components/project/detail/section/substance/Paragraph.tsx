b3f59382d4890c6f9db3198a880a225e4c5d5a31
Choi YuJin <yjyj0774@gmail.com>
2025-03-27 18:40:42 +0900

    컴포넌트 디자인 수정

--- src/components/project/detail/section/substance/Paragraph.tsx
+++ src/components/project/detail/section/substance/Paragraph.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ParagraphProps } from "../../../../../interfaces/components/project/detail/section/substance/Paragraph.interface";
 
 const Paragraph: React.FC<ParagraphProps> = ({ para, className }) => {
-  return <p className={` ${className}`}>{para}</p>;
+  return <p className={`mb-4 font-pretendard ${className}`}>{para}</p>;
 };
 
 export default Paragraph;
--- src/components/project/detail/section/substance/SubTitle.tsx
+++ src/components/project/detail/section/substance/SubTitle.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { SubTitleProps } from "../../../../../interfaces/components/project/detail/section/substance/SubTitle.interface";
 
 const SubTitle: React.FC<SubTitleProps> = ({ subTitle, className }) => {
-  return <p className={` ${className}`}>{subTitle}</p>;
+  return (
+    <p
+      className={`font-pretendardSemiBold text-black text-lg mb-2 ${className}`}
+    >
+      {subTitle}
+    </p>
+  );
 };
 
 export default SubTitle;
