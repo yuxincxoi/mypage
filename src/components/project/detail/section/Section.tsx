62e2ad9f52d4847926009be4d8326c61a38970f3
Choi YuJin <yjyj0774@gmail.com>
2025-03-27 18:01:16 +0900

    section 컴포넌트 생성

--- src/components/project/detail/section/Section.tsx
+++ src/components/project/detail/section/Section.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import Substance from "./substance/Substance";
+import { SectionProps } from "../../../../interfaces/components/project/detail/section/Section.interface";
+import {
+  BasicSubstance,
+  TroubleShootingSubstance,
+} from "./substance/Substance";
 
-const Section: React.FC = () => {
-  return <div></div>;
+const Section: React.FC<SectionProps> = ({
+  className,
+  sectionTitle,
+  isBasic,
+  para,
+  subTitle,
+}) => {
+  return (
+    <>
+      <p className={`text-3xl font-pretendardSemiBold text-black ${className}`}>
+        {sectionTitle}
+      </p>
+      {isBasic ? (
+        <BasicSubstance para={para} />
+      ) : (
+        <TroubleShootingSubstance subTitle={subTitle} para={para} />
+      )}
+    </>
+  );
 };
 
 export default Section;
---
+++ src/interfaces/components/project/detail/section/Section.interface.ts
@@ -0,0 +1,7 @@
+export interface SectionProps {
+  className?: string;
+  sectionTitle: string;
+  isBasic: boolean;
+  para: string;
+  subTitle?: string;
+}
