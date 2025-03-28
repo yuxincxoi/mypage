96a12d151caaf8177e3f0097bea0e64c7657579c
Choi YuJin <yjyj0774@gmail.com>
2025-03-28 16:14:05 +0900

    close modal button 컴포넌트 생성

---
+++ src/components/project/modal/CloseModalBtn.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { X } from "lucide-react";
+
+interface CloseModalBtnProps {
+  onClose: () => void;
+}
+
+const CloseModalBtn: React.FC<CloseModalBtnProps> = ({ onClose }) => {
+  return (
+    <button
+      aria-label="Close Modal"
+      className="absolute -top-7 right-0 text-white hover:text-gray-300 transition-colors duration-100"
+      onClick={onClose}
+    >
+      <X size={22} strokeWidth={2} />
+    </button>
+  );
+};
+
+export default CloseModalBtn;
