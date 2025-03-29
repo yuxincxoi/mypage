5c9a75a44f7bdf30c242da44f0aeb7cc2ca3beb3
Choi YuJin <yjyj0774@gmail.com>
2025-03-29 18:07:13 +0900

    이미지 추가

--- statics/imagePaths.ts
+++ statics/imagePaths.ts
@@ -3,6 +3,9 @@ export const imagePaths = {
     "/img/project1/img1.png",
     "/img/project1/img2.png",
     "/img/project1/img3.png",
+    "/img/project1/img4.png",
+    "/img/project1/img5.png",
+    "/img/project1/img6.png",
   ],
   project_2: [
     "/img/project2/img1.png",
@@ -23,11 +26,23 @@ export const imagePaths = {
     "/img/project3/img11.png",
     "/img/project3/img12.png",
   ],
+  project_6: [
+    "/img/project6/img1.png",
+    "/img/project6/img2.png",
+    "/img/project6/img3.png",
+    "/img/project6/img4.png",
+  ],
   project_7: [
     "/img/project7/img1.png",
     "/img/project7/img2.png",
     "/img/project7/img3.png",
     "/img/project7/img4.png",
+  ],
+  project_8: [
+    "/img/project8/img1.png",
+    "/img/project8/img2.png",
+    "/img/project8/img3.png",
+    "/img/project8/img4.png",
   ],
   project_9: ["/img/project9/img1.png", "/img/project9/img2.png"],
   project_10: ["/img/project10/img1.png", "/img/project10/img2.png"],
