132fef62beda4d903d269e59020f38b7bbb20814
Choi YuJin <yjyj0774@gmail.com>
2025-03-29 18:29:15 +0900

    모달 디자인 수정

--- src/components/project/modal/CloseModalBtn.tsx
+++ src/components/project/modal/CloseModalBtn.tsx
@@ -6,7 +6,7 @@ const CloseModalBtn: React.FC<CloseModalBtnProps> = ({ onClose }) => {
   return (
     <button
       aria-label="Close Modal"
-      className="absolute -top-7 right-0 text-white hover:text-gray-300 transition-colors duration-100"
+      className="absolute -top-7 right-0 text-neutral-400 hover:text-neutral-500 transition-colors duration-100"
       onClick={onClose}
     >
       <X size={22} strokeWidth={2} />
--- src/components/project/modal/Modal.tsx
+++ src/components/project/modal/Modal.tsx
@@ -31,7 +31,7 @@ const Modal: React.FC<ModalProps> = ({
       onClick={onClose}
     >
       <div
-        className="relative max-w-[90%] max-h-[90%] flex flex-col items-center"
+        className="relative w-[40vw] max-h-[90%] flex flex-col items-center"
         onClick={(e) => e.stopPropagation()}
       >
         <CloseModalBtn onClose={onClose} />
@@ -41,14 +41,14 @@ const Modal: React.FC<ModalProps> = ({
               direction="left"
               onClick={onPrev}
               isHidden={selectedImageIndex <= 0}
-              className="top-[20vh] text-white hover:text-gray-300"
+              className="top-[20vh] text-neutral-400 hover:text-neutral-500"
               strokeWidth={2}
             />
             <ChevronBtn
               direction="right"
               onClick={onNext}
               isHidden={selectedImageIndex >= images.length - 1}
-              className="top-[20vh] text-white hover:text-gray-300"
+              className="top-[20vh] text-neutral-400 hover:text-neutral-500"
               strokeWidth={2}
             />
           </>
@@ -58,7 +58,7 @@ const Modal: React.FC<ModalProps> = ({
           alt={`Project Image ${selectedImageIndex + 1}`}
           className="w-[50vw] h-[40vh] object-contain"
         />
-        <div className="text-center h-28">
+        <div className="w-[50vw] text-center h-28">
           <div className="font-pretendard mt-1">
             {comment[selectedImageIndex][0]}
           </div>
