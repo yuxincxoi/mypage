a88d851681598b588d04331ae6d89058677da8c3
Choi YuJin <yjyj0774@gmail.com>
2025-04-04 15:47:06 +0900

    특징 디자인 수정

--- src/components/project/detail/section/substance/Paragraph.tsx
+++ src/components/project/detail/section/substance/Paragraph.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { ParagraphProps } from "../../../../../interfaces/components/project/detail/section/substance/Paragraph.interface";
 
 const Paragraph: React.FC<ParagraphProps> = ({ para, className }) => {
-  return <p className={`mb-4 font-pretendard ${className}`}>{para}</p>;
+  return (
+    <ul className="list-disc list-outside marker:text-xs">
+      <li className={`mb-4 font-pretendard ${className}`}>{para}</li>
+    </ul>
+  );
 };
 
 export default Paragraph;
