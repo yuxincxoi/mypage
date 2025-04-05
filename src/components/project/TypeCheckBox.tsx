06dcdde97f9efae793bb11a3aea41154ecbef679
Choi YuJin <yjyj0774@gmail.com>
2025-04-05 18:40:33 +0900

    type checkbox 디자인 수정

--- src/components/project/TypeCheckBox.tsx
+++ src/components/project/TypeCheckBox.tsx
@@ -9,14 +9,16 @@ const TypeChechBox: React.FC<TypeCheckBoxProps> = ({
 }) => {
   return (
     <div
-      className={`flex w-72 text-black bg-neutral-100 justify-between text-md px-2 py-2 rounded-full fixed top-5 left-1/2 transform -translate-x-1/2 z-50 transition-opacity duration-500 ${
+      className={`flex w-72 text-zinc-500 bg-neutral-100 justify-between text-md px-2 py-2 rounded-full fixed top-5 left-1/2 transform -translate-x-1/2 z-50 transition-opacity duration-500 ${
         isVisible ? "opacity-95" : "opacity-0 pointer-events-none"
       }`}
     >
       <div>
         <div
-          className={`cursor-pointer font-pretendard transition-all duration-500 ease-in-out w-16 h-10 text-center flex items-center justify-center ${
-            selectedType === "all" ? "rounded-full bg-white" : "text-zinc-500"
+          className={`cursor-pointer font-pretendard transition-all duration-300 ease-in-out w-16 h-10 text-center flex items-center justify-center ${
+            selectedType === "all"
+              ? "rounded-full bg-white text-zinc-800"
+              : "hover:text-zinc-700"
           }`}
           onClick={() => onTypeChange("all")}
         >
@@ -25,10 +27,10 @@ const TypeChechBox: React.FC<TypeCheckBoxProps> = ({
       </div>
       <div>
         <div
-          className={`cursor-pointer font-pretendard transition-all duration-500 ease-in-out w-24 h-10 text-center flex items-center justify-center ${
+          className={`cursor-pointer font-pretendard transition-all duration-300 ease-in-out w-24 h-10 text-center flex items-center justify-center ${
             selectedType === "personal"
-              ? "rounded-full bg-white"
-              : "text-zinc-500"
+              ? "rounded-full bg-white text-zinc-800"
+              : "hover:text-zinc-700"
           }`}
           onClick={() => onTypeChange("personal")}
         >
@@ -37,8 +39,10 @@ const TypeChechBox: React.FC<TypeCheckBoxProps> = ({
       </div>
       <div>
         <div
-          className={`cursor-pointer font-pretendard transition-all duration-500 ease-in-out w-20 h-10 text-center flex items-center justify-center ${
-            selectedType === "team" ? "rounded-full bg-white" : "text-zinc-500"
+          className={`cursor-pointer font-pretendard transition-all duration-300 ease-in-out w-20 h-10 text-center flex items-center justify-center ${
+            selectedType === "team"
+              ? "rounded-full bg-white text-zinc-800"
+              : "hover:text-zinc-700"
           }`}
           onClick={() => onTypeChange("team")}
         >
