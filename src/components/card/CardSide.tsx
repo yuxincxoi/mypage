932c47c42748982208eebaadea55cf0fe548e68e
Choi YuJin <yjyj0774@gmail.com>
2025-04-05 19:15:57 +0900

    카드 그림자 색상 변경

--- src/components/card/CardSide.tsx
+++ src/components/card/CardSide.tsx
@@ -4,7 +4,7 @@ import { CardSideProps } from "../../interfaces/components/card/CardSide.interfa
 const CardSide: React.FC<CardSideProps> = ({ isBack, children }) => {
   return (
     <div
-      className={`absolute w-full h-full rounded-lg shadow-2xl shadow-neutral-500 p-6 flex flex-col justify-between text-white bg-gradient-to-r from-amber-400 from-10% via-orange-400 via-70% to-rose-400`}
+      className={`absolute w-full h-full rounded-lg shadow-2xl shadow-neutral-400 p-6 flex flex-col justify-between text-white bg-gradient-to-r from-amber-400 from-10% via-orange-400 via-70% to-rose-400`}
       style={{
         backfaceVisibility: "hidden",
         transform: isBack ? "rotateY(180deg)" : "rotateY(0deg)",
