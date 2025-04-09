2d06d07d98df3f7806e45f90db0cb80d1d15aba1
Choi YuJin <yjyj0774@gmail.com>
2025-04-09 12:37:48 +0900

    오버레이 이미지 설정

--- src/components/project/ProjectIcon.tsx
+++ src/components/project/ProjectIcon.tsx
@@ -14,22 +14,31 @@ const ProjectIcon: React.FC<ProjectIconProps> = ({
   return (
     <div
       className={`w-80 h-60 rounded-lg flex items-center justify-center flex-col group border-[1px] border-zinc-300 hover:shadow-lg transition-shadow duration-300
-        ${
-          isBlurred
-            ? "opacity-90 blur-lg cursor-default"
-            : "opacity-100 blur-none cursor-pointer"
-        } transition-opacity duration-1000 ease-in-out`}
+    ${
+      isBlurred
+        ? "opacity-90 blur-lg cursor-default"
+        : "opacity-100 blur-none cursor-pointer"
+    } overflow-hidden relative`}
       onClick={isBlurred ? undefined : onClick}
       data-type={projectType}
     >
-      <div className="w-full h-full p-8 relative">
+      {/* 배경 이미지 */}
+      <div
+        className={`absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-50  transition-all duration-500`}
+        style={{
+          backgroundImage: `url(/img/project${projectId}/cover.png)`,
+        }}
+      ></div>
+      {/* 오버레이 */}
+      <div className="absolute inset-0 bg-zinc-100 opacity-0 group-hover:opacity-30 transition-opacity duration-500"></div>
+      {/* 내용 */}
+      <div className="w-full h-full p-8 relative z-10">
         <div className="font-pretendardSemiBold text-2xl text-black">
           {projectTitle}
         </div>
         <div className="font-pretendardExtraLight text-md text-zinc-500">
           {projectSubTitle}
         </div>
-
         <div className="w-16 h-16 bg-zinc-300 absolute bottom-10 right-10"></div>
       </div>
     </div>
