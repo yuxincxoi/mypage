d03afd5573319525db53298023c0b41b61dd766f
Choi YuJin <yjyj0774@gmail.com>
2025-04-09 11:36:09 +0900

    hover 효과 삭제

--- src/components/project/ProjectIcon.tsx
+++ src/components/project/ProjectIcon.tsx
@@ -31,47 +31,7 @@ const ProjectIcon: React.FC<ProjectIconProps> = ({
       }}
       onClick={isBlurred ? undefined : onClick}
       data-type={projectType}
-    >
-      <div className="w-52 h-52 rounded-full shadow-lg absolute top-0 left-0" />
-      <svg
-        width="220"
-        height="220"
-        viewBox="0 0 220 220"
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-        style={{
-          overflow: "visible",
-          display: isBlurred ? "none" : "block",
-        }}
-      >
-        <circle
-          cx={centerX}
-          cy={centerY}
-          r={radius}
-          fill="none"
-          stroke="white"
-          strokeWidth="2"
-          strokeOpacity="0.7"
-        />
-        <path
-          id={`textPath-${projectId}`}
-          d={`M ${centerX - radius}, ${centerY} A ${radius},${radius} 0 0,1 ${
-            centerX + radius
-          },${centerY}`}
-          fill="none"
-          stroke="none"
-        />
-
-        <text className="font-pretendard text-black text-sm">
-          <textPath
-            xlinkHref={`#textPath-${projectId}`}
-            startOffset="50%"
-            textAnchor="middle"
-          >
-            {projectTitle} ::: {projectSubTitle}
-          </textPath>
-        </text>
-      </svg>
-    </div>
+    ></div>
   );
 };
 
