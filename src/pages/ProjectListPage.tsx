ab62f717df71849e5973fb07abebe18a0fa7823c
Choi YuJin <yjyj0774@gmail.com>
2025-04-09 11:40:48 +0900

    icon 배치 설정 코드 삭제

--- src/pages/ProjectListPage.tsx
+++ src/pages/ProjectListPage.tsx
@@ -9,16 +9,6 @@ import { Project } from "../interfaces/components/project/Project.interface";
 
 const ProjectListPage: React.FC = () => {
   const [selectedType, setSelectedType] = useState<string>("all");
-  const [bubblePositions, setBubblePositions] = useState<
-    {
-      id: number;
-      style: {
-        left: string;
-        top: string;
-        animation: string;
-      };
-    }[]
-  >([]);
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
   const [isProjectDetailVisible, setIsProjectDetailVisible] =
     useState<boolean>(false);
@@ -52,44 +42,6 @@ const ProjectListPage: React.FC = () => {
       };
     })
   );
-
-  // 고정된 위치를 지정한 배열
-  const fixedPositions = [
-    { x: 120, y: 10 },
-    { x: 280, y: 220 },
-    { x: 5, y: 250 },
-    { x: 460, y: 30 },
-    { x: 500, y: 390 },
-    { x: 780, y: 450 },
-    { x: 680, y: 200 },
-    { x: 850, y: 10 },
-    { x: 160, y: 450 },
-    { x: 980, y: 250 },
-  ];
-
-  // 각 프로젝트 아이콘 위치 및 애니메이션 설정
-  const generateBubblePositions = () => {
-    return projects.map((project, index) => {
-      const { x, y } = fixedPositions[index];
-
-      // 애니메이션
-      const floatDuration = 3 + Math.random() * 4;
-      const delay = Math.random() * 5;
-
-      return {
-        id: project.id,
-        style: {
-          left: `${x}px`,
-          top: `${y}px`,
-          animation: `float ${floatDuration}s ease-in-out ${delay}s infinite alternate`,
-        },
-      };
-    });
-  };
-
-  useEffect(() => {
-    setBubblePositions(generateBubblePositions());
-  }, [projects.length]);
 
   const handleIconClick = (projectId: number) => {
     const projectData = projects.find((p) => p.id === projectId);
