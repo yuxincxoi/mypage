93a846dd0ce489e165a809abaf9bd9c011af3ce9
Choi YuJin <yjyj0774@gmail.com>
2025-04-12 16:26:08 +0900

    icon 디자인 수정

--- src/components/project/ProjectIcon.tsx
+++ src/components/project/ProjectIcon.tsx
@@ -14,33 +14,26 @@ const ProjectIcon: React.FC<ProjectIconProps> = ({
   return (
     <div
       className={`w-60 h-80 rounded-lg flex items-center justify-center flex-col group border-[1px] border-zinc-300 hover:shadow-lg transition-shadow duration-300
-    ${
-      isBlurred
-        ? "opacity-90 blur-lg cursor-default"
-        : "opacity-100 blur-none cursor-pointer"
-    } overflow-hidden relative break-inside-avoid mb-4`}
+  ${
+    isBlurred
+      ? "opacity-90 blur-lg cursor-default"
+      : "opacity-100 blur-none cursor-pointer"
+  }
+  relative break-inside-avoid mb-4`}
       style={style}
       onClick={isBlurred ? undefined : onClick}
       data-type={projectType}
     >
-      {/* 배경 이미지 */}
-      <div
-        className={`absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-50  transition-all duration-500`}
-        style={{
-          backgroundImage: `url(/img/project${projectId}/cover.png)`,
-        }}
-      ></div>
-      {/* 오버레이 */}
-      <div className="absolute inset-0 bg-zinc-100 opacity-0 group-hover:opacity-30 transition-opacity duration-500"></div>
-      {/* 내용 */}
-      <div className="w-full h-full p-8 relative z-10">
+      <div className="w-40 h-40 bg-zinc-300 absolute -bottom-10 -right-10 opacity-0 group-hover:opacity-100 ease-in-out transition-all duration-700 pointer-events-none z-20"></div>
+      <div className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
+      <div className="w-full h-full p-8 relative z-10 overflow-hidden">
         <div className="font-pretendardSemiBold text-2xl text-black">
           {projectTitle}
         </div>
         <div className="font-pretendardExtraLight text-md text-zinc-500">
           {projectSubTitle}
         </div>
-        <div className="w-16 h-16 bg-zinc-300 absolute bottom-10 right-10"></div>
+        <div className="w-40 h-40 bg-zinc-300 absolute -bottom-10 -right-10"></div>
       </div>
     </div>
   );
