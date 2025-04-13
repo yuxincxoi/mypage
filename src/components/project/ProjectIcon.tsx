704d53178182eabb8912a6761c42ecece52e83d6
Choi YuJin <yjyj0774@gmail.com>
2025-04-13 16:03:59 +0900

    icon 디자인 수정

--- src/components/project/ProjectIcon.tsx
+++ src/components/project/ProjectIcon.tsx
@@ -13,7 +13,7 @@ const ProjectIcon: React.FC<ProjectIconProps> = ({
 }) => {
   return (
     <div
-      className={`w-60 h-[270px] rounded-lg flex items-center justify-center flex-col group border-[1px] border-zinc-300 hover:shadow-lg transition-shadow duration-300 z-10
+      className={`w-60 h-[230px] rounded-lg flex items-center justify-center flex-col group border-[1px] border-zinc-300 hover:shadow-lg transition-shadow duration-300 z-10
   ${
     isBlurred
       ? "opacity-90 blur-lg cursor-default"
@@ -25,21 +25,21 @@ const ProjectIcon: React.FC<ProjectIconProps> = ({
       data-type={projectType}
     >
       <div
-        className="w-44 h-44 absolute bg-contain bg-no-repeat bg-center -bottom-6 -right-8 opacity-0 group-hover:opacity-100 ease-in-out transition-all duration-500 pointer-events-none z-20"
+        className="w-44 h-44 absolute bg-contain bg-no-repeat bg-center -bottom-7 -right-8 opacity-0 group-hover:opacity-100 ease-in-out transition-all duration-500 pointer-events-none z-20"
         style={{
           backgroundImage: `url(/img/project${projectId}/cover.png)`,
         }}
       ></div>
       <div className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
-      <div className="w-full h-full p-8 relative z-10 overflow-hidden">
+      <div className="w-full h-full px-6 py-5 relative z-10 overflow-hidden">
         <div className="font-pretendardSemiBold text-2xl text-black">
           {projectTitle}
         </div>
-        <div className="font-pretendardExtraLight text-md text-zinc-500">
+        <div className="font-pretendardExtraLight text-md leading-5 text-zinc-500 whitespace-pre-wrap">
           {projectSubTitle}
         </div>
         <div
-          className="w-44 h-44 absolute -bottom-6 -right-8 bg-contain bg-no-repeat bg-center"
+          className="w-44 h-44 absolute -bottom-7 -right-8 bg-contain bg-no-repeat bg-center"
           style={{
             backgroundImage: `url(/img/project${projectId}/cover.png)`,
           }}
