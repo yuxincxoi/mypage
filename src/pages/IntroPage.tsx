532aa89bc0f2f93048188ac81393793c68c4b596
Choi YuJin <yjyj0774@gmail.com>
2025-06-09 21:15:42 +0900

    배경 이미지 추가

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -44,7 +44,29 @@ const IntroPage: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen mb-40 pb-32 flex flex-col justify-center">
+    <div className="w-full h-screen mb-40 pb-32 flex flex-col justify-center relative">
+      <div className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none">
+        <img
+          className="w-80 bg-contain absolute bg-no-repeat bg-center top-[4%] left-[9%]"
+          src="/img/miffy.gif"
+          alt=""
+        />
+        <img
+          className="w-80 bg-contain absolute bg-no-repeat bg-center top-[10%] left-[70%]"
+          src="/img/test2.gif"
+          alt=""
+        />
+        <img
+          className="w-80 bg-contain absolute bg-no-repeat bg-center bottom-[15%] right-[15%]"
+          src="/img/cherry.png"
+          alt=""
+        />
+        <img
+          className="w-96 bg-contain absolute bg-no-repeat bg-center bottom-[14%] left-[3%]"
+          src="/img/test.png"
+          alt=""
+        />
+      </div>
       <div className="font-pretendardBold text-[200px] font-extrabold leading-[200px] tracking-tight text-white text-stroke whitespace-pre-wrap relative">
         <div className="relative">
           {/* 첫 번째 파트 */}
