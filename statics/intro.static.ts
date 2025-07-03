2c9d71f56b176ccbb3f402c6a09dbdb40bc23645
Choi YuJin <yjyj0774@gmail.com>
2025-07-03 16:52:01 +0900

    intro message 수정

--- statics/intro.static.ts
+++ statics/intro.static.ts
@@ -1,5 +1,7 @@
 export const introMessage = {
-  nameFirst: `반갑습니다`,
-  nameSecond: `최유진입니다`,
-  job: "Front-end Developer",
+  introFirst: `반갑습니다 !`,
+  introSecond: `저는 최유진입니다.`,
+  introThird: "Front-end Developer 입니다.",
+  introFourth: "좋아하는 것으로 채운 이 공간이 당신에게도 스며들기를,",
+  introFifth: "조용한 영감이 되기를 바랍니다.",
 };
