ef334bbdd020630f6a1d7c3eabde0f4925331f1e
Choi YuJin <yjyj0774@gmail.com>
2025-07-05 17:47:00 +0900

    로딩화면 수정

--- src/components/intro/SplashScreen.tsx
+++ src/components/intro/SplashScreen.tsx
@@ -6,17 +6,29 @@ type SplashScreenProps = {
 
 const SplashScreen: React.FC<SplashScreenProps> = ({ onFinish }) => {
   const [visible, setVisible] = useState(true);
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
+    const progressInterval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(progressInterval);
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 60);
+
     const appearTimer = setTimeout(() => {
       setVisible(false);
-    }, 1500);
+    }, 3000);
 
     const finishTimer = setTimeout(() => {
       onFinish(); // 사라진 뒤 IntroPage로 전환
     }, 2500);
 
     return () => {
+      clearInterval(progressInterval);
       clearTimeout(appearTimer);
       clearTimeout(finishTimer);
     };
@@ -28,8 +40,18 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ onFinish }) => {
         visible ? "opacity-100" : "opacity-0"
       }`}
     >
-      <div className="text-2xl tracking-widest font-shirik font-extrabold animate-pulse scale-x-90">
-        CHOiYUJiN
+      <div className="text-2xl tracking-widest font-shirik font-extrabold animate-pulse scale-x-90 relative overflow-hidden">
+        <div className="text-zinc-300">CHOiYUJiN</div>
+
+        {/* 텍스트 오버레이 */}
+        <div
+          className="absolute top-0 left-0 text-black overflow-hidden"
+          style={{
+            height: `${progress}%`,
+          }}
+        >
+          CHOiYUJiN
+        </div>
       </div>
     </div>
   );
